1f713937d6249cb24b487e236aa9c8cd5904e80e
ASlugin <slugin.alexander@gmail.com>
2022-02-24 01:11:47 +0300

    Added entering an array

---
+++ Homework1/Sorting/Sorting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputString = Console.ReadLine();
+            if (inputString == null)
+            {
+                return;
+            }
+            string[] nums = inputString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int[] inputArray = new int[nums.Length];
+            for (var i = 0; i < nums.Length; i++)
+            {
+                inputArray[i] = int.Parse(nums[i]);
+            }
+
+            foreach (var i in inputArray)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
