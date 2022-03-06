dc7c7ab3f9b4396a3a4ed539e9b89e28697a5d29
ASlugin <slugin.alexander@gmail.com>
2022-03-06 18:28:05 +0300

    Add reverse Burrows-Wheeler transform

--- BWT/BWT/Program.cs
+++ BWT/BWT/Program.cs
@@ -49,20 +49,85 @@ namespace BWT
             return result;
         }
 
+        public static void sortArrayChar(char[] inputString)
+        {
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                for (int j = 0; j < inputString.Length - i - 1; j++)
+                {
+                    if (inputString[j] > inputString[j + 1])
+                    {
+                        char temp = inputString[j];
+                        inputString[j] = inputString[j + 1];
+                        inputString[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Performs a reverse Burrows-Wheeler transformation
+        /// </summary>
+        /// <param name="input">String that was converted by burrows-wheeler transformation</param>
+        /// <param name="positionOfEnd">Position of the end of source string in string</param>
+        /// <returns></returns>
+        public static string ReverseBWT(string input, int positionOfEnd)
+        {
+            char sybmolNotFromString = (char)1;
+            while (input.Contains(sybmolNotFromString))
+            {
+                sybmolNotFromString++;
+            }
+
+            char[] charsOfString = input.ToCharArray();
+            sortArrayChar(charsOfString);
+            string stringAfterSort = string.Concat(charsOfString);
+
+            int[] indexOfNextSymbol = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                indexOfNextSymbol[i] = stringAfterSort.IndexOf(input[i]);
+                stringAfterSort = stringAfterSort.Remove(indexOfNextSymbol[i], 1);
+                stringAfterSort = stringAfterSort.Insert(indexOfNextSymbol[i], sybmolNotFromString.ToString());
+            }
+
+            string inverseToResult = input[positionOfEnd].ToString();
+            int j = indexOfNextSymbol[positionOfEnd];
+            while (inverseToResult.Length < input.Length)
+            {
+                inverseToResult = inverseToResult + input[j];
+                j = indexOfNextSymbol[j];
+            }
+
+            return String.Concat(inverseToResult.ToCharArray().Reverse());
+        }
+
         public static void Main(string[] args)
         {
             Console.Write("Enter a string for Burrows-Wheeler tranformation: ");
             var inputString = Console.ReadLine();
             if (inputString == null)
+            {
                 return;
+            }
 
             int positionOfEnd = 0;
             string stringAfterBWT = DirectBWT(inputString, ref positionOfEnd);
-            Console.Write("String after Burrows-Wheeler transformation: ");
-            Console.WriteLine(stringAfterBWT);
-            Console.Write("Position of the end of source string in string after BWT: ");
-            Console.WriteLine(positionOfEnd);
+            Console.WriteLine($"String after Burrows-Wheeler transformation: {stringAfterBWT}");
+            Console.WriteLine($"Position of the end of source string in string after BWT: {positionOfEnd}");
             
+
+            string stringAfterReverseBWT = ReverseBWT(stringAfterBWT, positionOfEnd);
+            Console.WriteLine($"String after reverse Burrows-Wheeler transformation: {stringAfterReverseBWT}");
+
+            if (string.Compare(stringAfterReverseBWT, inputString) == 0)
+            {
+                Console.WriteLine("Burrows-Wheeler transformation was performed correctly");
+            }
+            else
+            {
+                Console.WriteLine("Burrows-Wheeler transformation was performed incorrectly");
+            }
         }
     }
 }
