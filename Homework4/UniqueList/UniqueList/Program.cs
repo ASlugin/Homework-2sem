098b06d097d9eb1da6f0a6120bacefe0fc343a85
ASlugin <slugin.alexander@gmail.com>
2022-03-24 21:25:44 +0300

    Add first version UniqueList

---
+++ Homework4/UniqueList/UniqueList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniqueList
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
