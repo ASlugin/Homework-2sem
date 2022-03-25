74e55443fddf37338ecab7981c65ad5be5bf2b6e
ASlugin <slugin.alexander@gmail.com>
2022-03-26 01:30:22 +0300

    Added methods for list

---
+++ Homework4/UniqueList/UniqueList/List.cs
@@ -0,0 +1,128 @@
+﻿namespace UniqueList
+{
+    public class List
+    {
+        public List()
+        {
+        }
+
+        private class ListElement
+        {
+            public ListElement(int newValue, ListElement? next)
+            {
+                this.value = newValue;
+                this.next = next;
+            }
+            public int value;
+            public ListElement? next;
+        }
+
+        private ListElement? head;
+
+        /// <summary>
+        /// Amount of elements in the list
+        /// </summary>
+        public int Size { get; private set; } = 0;
+
+        /// <summary>
+        /// Check whether the list is empty
+        /// </summary>
+        /// <returns>If the list is empty return true, else return false</returns>
+        public bool IsEmpty()
+            => Size == 0;
+
+        private ListElement FindElementByPosition(int position)
+        {
+            if (head == null)
+            {
+                throw new InvalidOperationException();
+            }
+            ListElement current = head;
+            for (int i = 0; i < position; i++)
+            {
+                if (current == null || current.next == null)
+                {
+                    throw new NullReferenceException();
+                }
+                current = current.next;
+            }
+            return current;
+        }
+
+        /// <summary>
+        /// Adds a new element to given position of the list
+        /// </summary>
+        /// <param name="value">Value of new element</param>
+        /// <param name="position">Position for new element</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void Add(int value, int position)
+        {
+            if (position < 0 || position > Size)
+            {
+                throw new ArgumentOutOfRangeException("Invalid position");
+            }
+
+            if (position == 0 || head == null)
+            {
+                head = new ListElement(value, head);
+                Size++;
+                return;
+            }
+            ListElement elementBeforeNewElement = FindElementByPosition(position - 1);
+            elementBeforeNewElement.next = new ListElement(value, elementBeforeNewElement.next);
+            Size++;
+        }
+
+        /// <summary>
+        /// Deletes an element at the given position from the list 
+        /// </summary>
+        /// <param name="position">Position to delete element</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void Delete(int position)
+        {
+            if (position < 0 || position >= Size)
+            {
+                throw new ArgumentOutOfRangeException("Попытка удаления несуществуюшего элемента");     // Реализовать класс исключений
+            }
+
+            if (position == 0)
+            {
+                head = head?.next;
+                Size--;
+                return;
+            }
+            ListElement elementBeforeElementForDelete = FindElementByPosition(position - 1);
+            elementBeforeElementForDelete.next = elementBeforeElementForDelete.next?.next;
+            Size--;
+        }
+
+        /// <summary>
+        /// Changes value of the element at the given position
+        /// </summary>
+        /// <param name="newValue">New value for the element</param>
+        /// <param name="position">Зosition for the element to be changed</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void ChangeValueOfElement(int newValue, int position)
+        {
+            if (position < 0 || position >= Size)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            FindElementByPosition(position).value = newValue;
+        }
+
+        /// <summary>
+        /// Returns value of element at the given position
+        /// </summary>
+        /// <param name="position">Position for the element to return its value</param>
+        /// <returns></returns>
+        public int GetValue(int position)
+        {
+            if (position < 0 || position >= Size)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return FindElementByPosition(position).value;
+        }
+    }
+}
--- Homework4/UniqueList/UniqueList/Program.cs
+++ Homework4/UniqueList/UniqueList/Program.cs
@@ -6,7 +6,23 @@ namespace UniqueList
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List list = new List();
+            list.Add(11, list.Size);
+            list.Add(22, list.Size);
+            list.Add(33, list.Size);
+
+            list.ChangeValueOfElement(12, 0);
+            list.ChangeValueOfElement(23, 1);
+            list.ChangeValueOfElement(34, list.Size - 1);
+
+            for (int i = 0; i < list.Size; i++)
+            {
+                Console.WriteLine(list.GetValue(i));
+            }
+
+            list.Delete(1);
+            list.Delete(0);
+            list.Delete(list.Size - 1);
         }
     }
 }
