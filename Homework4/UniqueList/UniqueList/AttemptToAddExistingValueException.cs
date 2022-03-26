71c43c2bed24efe8f2b9191d80cb0849210531c8
ASlugin <slugin.alexander@gmail.com>
2022-03-26 13:27:21 +0300

    Added throws classes files

---
+++ Homework4/UniqueList/UniqueList/AttemptToAddExistingValueException.cs
@@ -0,0 +1,13 @@
+﻿namespace UniqueList
+{
+    [Serializable]
+    public class AttemptToAddExistingValueException : Exception
+    {
+        public AttemptToAddExistingValueException() { }
+        public AttemptToAddExistingValueException(string message) : base(message) { }
+        public AttemptToAddExistingValueException(string message, Exception inner) : base(message, inner) { }
+        protected AttemptToAddExistingValueException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ Homework4/UniqueList/UniqueList/AttemptToDeleteNonexistentElement.cs
@@ -0,0 +1,13 @@
+﻿namespace UniqueList
+{
+    [Serializable]
+    public class AttemptToDeleteNonexistentElement : Exception
+    {
+        public AttemptToDeleteNonexistentElement() { }
+        public AttemptToDeleteNonexistentElement(string message) : base(message) { }
+        public AttemptToDeleteNonexistentElement(string message, Exception inner) : base(message, inner) { }
+        protected AttemptToDeleteNonexistentElement(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
