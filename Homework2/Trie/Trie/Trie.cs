7582bde839ee713bfbae080b0657eda672687a71
ASlugin <slugin.alexander@gmail.com>
2022-04-03 12:28:58 +0300

    Fixed namespace

--- Homework2/Trie/Trie/Trie.cs
+++ Homework2/Trie/Trie/Trie.cs
@@ -1,167 +1,165 @@
-﻿using System;
+﻿namespace Trie;
+
+using System;
 using System.Collections.Generic;
 
-namespace Trie
+class Trie
 {
-    class Trie
+    public Trie()
     {
-        public Trie()
+        this.root = new Element();
+    }
+
+    private Element root;
+    private class Element
+    {
+        public Element()
         {
-            this.root = new Element();
+            this.NextElements = new Dictionary<char, Element>();
+            this.Terminal = false;
         }
+        public Dictionary<char, Element> NextElements;
+        public bool Terminal;
+    }
 
-        private Element root;
-        private class Element
+    /// <summary>
+    /// Amount of word in trie
+    /// </summary>
+    public int Size { get; private set; } = 0;
+
+    /// <summary>
+    /// Adds a string to trie
+    /// </summary>
+    /// <param name="element">String to add to trie</param>
+    /// <returns>True if there has not been such a string yet</returns>
+    public bool Add(string element)
+    {
+        var currentElement = root;
+        for (var i = 0; i < element.Length; i++)
         {
-            public Element()
+            if (currentElement.NextElements.ContainsKey(element[i]))
             {
-                this.NextElements = new Dictionary<char, Element>();
-                this.Terminal = false;
-            }
-            public Dictionary<char, Element> NextElements;
-            public bool Terminal;
-        }
-
-        /// <summary>
-        /// Amount of word in trie
-        /// </summary>
-        public int Size { get; private set; } = 0;
-
-        /// <summary>
-        /// Adds a string to trie
-        /// </summary>
-        /// <param name="element">String to add to trie</param>
-        /// <returns>True if there has not been such a string yet</returns>
-        public bool Add(string element)
-        {
-            var currentElement = root;
-            for (int i = 0; i < element.Length; i++)
-            {
-                if (currentElement.NextElements.ContainsKey(element[i]))
+                if (i == element.Length - 1)
                 {
-                    if (i == element.Length - 1)
+                    if (!currentElement.NextElements[element[i]].Terminal)
                     {
-                        if (!currentElement.NextElements[element[i]].Terminal)
-                        {
-                            Size++;
-                            currentElement.NextElements[element[i]].Terminal = true;
-                            return true;
-                        }
-                        return false;
-                    }
-                    currentElement = currentElement.NextElements[element[i]];
-                }
-                else
-                {
-                    currentElement.NextElements.Add(element[i], new Element());
-                    if (i == element.Length - 1)
-                    {
-                        currentElement.NextElements[element[i]].Terminal = true;
                         Size++;
+                        currentElement.NextElements[element[i]].Terminal = true;
                         return true;
                     }
-                    currentElement = currentElement.NextElements[element[i]];
-                }
-            }
-            return false;
-        }
-
-        /// <summary>
-        /// Checks whether a string is contained in trie
-        /// </summary>
-        /// <param name="element">String to looking for in trie</param>
-        /// <returns>True if such string is contained in trie, else returns false</returns>
-        public bool Contains(string element)
-        {
-            var currentElement = root;
-            for (int i = 0; i < element.Length; ++i)
-            {
-                if (currentElement.NextElements.ContainsKey(element[i]))
-                {
-                    currentElement = currentElement.NextElements[element[i]];
-                    if (i == element.Length - 1 && currentElement.Terminal)
-                    {
-                        return true;
-                    }
-                }
-                else
-                {
                     return false;
                 }
+                currentElement = currentElement.NextElements[element[i]];
             }
-            return false;
-        }
-
-        /// <summary>
-        /// Removes given string from trie 
-        /// </summary>
-        /// <param name="element">String to remove from trie</param>
-        /// <returns>True if such string was in trie, else returns false</returns>
-        public bool Remove(string element)
-        {
-            bool removeThisElement = false;
-            return RemoveRecursive(root, element, ref removeThisElement);
-        }
-
-        private bool RemoveRecursive(Element currentElement, string element, ref bool removeThisElement)
-        {
-            if (element.Length == 0)
+            else
             {
-                if (currentElement.Terminal)
+                currentElement.NextElements.Add(element[i], new Element());
+                if (i == element.Length - 1)
                 {
-                    Size--;
-                    currentElement.Terminal = false;
-                    removeThisElement = currentElement.NextElements.Count == 0 ? true : false;
+                    currentElement.NextElements[element[i]].Terminal = true;
+                    Size++;
                     return true;
                 }
+                currentElement = currentElement.NextElements[element[i]];
+            }
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Checks whether a string is contained in trie
+    /// </summary>
+    /// <param name="element">String to looking for in trie</param>
+    /// <returns>True if such string is contained in trie, else returns false</returns>
+    public bool Contains(string element)
+    {
+        var currentElement = root;
+        for (var i = 0; i < element.Length; ++i)
+        {
+            if (currentElement.NextElements.ContainsKey(element[i]))
+            {
+                currentElement = currentElement.NextElements[element[i]];
+                if (i == element.Length - 1 && currentElement.Terminal)
+                {
+                    return true;
+                }
+            }
+            else
+            {
                 return false;
             }
+        }
+        return false;
+    }
 
-            if (currentElement.NextElements.ContainsKey(element[0]))
+    /// <summary>
+    /// Removes given string from trie 
+    /// </summary>
+    /// <param name="element">String to remove from trie</param>
+    /// <returns>True if such string was in trie, else returns false</returns>
+    public bool Remove(string element)
+    {
+        bool removeThisElement = false;
+        return RemoveRecursive(root, element, ref removeThisElement);
+    }
+
+    private bool RemoveRecursive(Element currentElement, string element, ref bool removeThisElement)
+    {
+        if (element.Length == 0)
+        {
+            if (currentElement.Terminal)
             {
-                if (!RemoveRecursive(currentElement.NextElements[element[0]],element.Substring(1), ref removeThisElement))
-                {
-                    return false;
-                }
-                if (removeThisElement)
-                {
-                    currentElement.NextElements.Remove(element[0]);
-                    removeThisElement = currentElement.NextElements.Count == 0 && !currentElement.Terminal ? true : false;
-                }
+                Size--;
+                currentElement.Terminal = false;
+                removeThisElement = currentElement.NextElements.Count == 0 ? true : false;
                 return true;
             }
-
             return false;
         }
 
-        /// <summary>
-        /// Counts how many words starts with a given prefix
-        /// </summary>
-        /// <param name="prefix"></param>
-        /// <returns>Amount word which starts with given prefix</returns>
-        public int HowManyStartsWithPrefix(string prefix)
+        if (currentElement.NextElements.ContainsKey(element[0]))
         {
-            var currentElement = root;
-            for (var i = 0; i < prefix.Length; ++i)
+            if (!RemoveRecursive(currentElement.NextElements[element[0]], element.Substring(1), ref removeThisElement))
             {
-                if (!currentElement.NextElements.ContainsKey(prefix[i]))
-                {
-                    return 0;
-                }
-                currentElement = currentElement.NextElements[prefix[i]];
+                return false;
             }
-            return terminalElementCounter(currentElement);
+            if (removeThisElement)
+            {
+                currentElement.NextElements.Remove(element[0]);
+                removeThisElement = currentElement.NextElements.Count == 0 && !currentElement.Terminal ? true : false;
+            }
+            return true;
         }
 
-        private int terminalElementCounter(Element currentElement)
-        {
-            int result = currentElement.Terminal ? 1 : 0;
-            foreach (var i in currentElement.NextElements)
-            {
-                result += terminalElementCounter(currentElement.NextElements[i.Key]);
-            }
-            return result;
-        }
+        return false;
+    }
 
+    /// <summary>
+    /// Counts how many words starts with a given prefix
+    /// </summary>
+    /// <param name="prefix"></param>
+    /// <returns>Amount word which starts with given prefix</returns>
+    public int HowManyStartsWithPrefix(string prefix)
+    {
+        var currentElement = root;
+        for (var i = 0; i < prefix.Length; ++i)
+        {
+            if (!currentElement.NextElements.ContainsKey(prefix[i]))
+            {
+                return 0;
+            }
+            currentElement = currentElement.NextElements[prefix[i]];
+        }
+        return terminalElementCounter(currentElement);
+    }
+
+    private int terminalElementCounter(Element currentElement)
+    {
+        int result = currentElement.Terminal ? 1 : 0;
+        foreach (var i in currentElement.NextElements)
+        {
+            result += terminalElementCounter(currentElement.NextElements[i.Key]);
+        }
+        return result;
     }
 }
