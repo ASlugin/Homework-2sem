05960ebf14eb0f871cf10e0977f68812cba4b63e
ASlugin <slugin.alexander@gmail.com>
2022-04-21 21:52:12 +0300

    Added first version of Calculator

---
+++ Homework7/Calculator/Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public class MyForm : Form
+    {
+        public Button button1;
+        public MyForm()
+        {
+            button1 = new Button();
+            button1.Size = new Size(40, 40);
+            button1.Location = new Point(30, 30);
+            button1.Text = "Click me";
+            this.Controls.Add(button1);
+            button1.Click += new EventHandler(Button1Click);
+        }
+        private void Button1Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new MyForm());
+        }
+    }
+
+}
