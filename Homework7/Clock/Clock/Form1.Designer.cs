5e115acb2f15a7f8f608a11f16ba00880336f89a
ASlugin <slugin.alexander@gmail.com>
2022-04-30 16:15:37 +0300

    Clock on new version dotnet

--- Homework7/Clock/Clock/Form1.Designer.cs
+++ Homework7/Clock/Clock/Form1.Designer.cs
@@ -1,16 +1,16 @@
 ﻿namespace Clock
 {
-    partial class Form1
+    partial class ClockForm
     {
         /// <summary>
-        /// Обязательная переменная конструктора.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Освободить все используемые ресурсы.
+        ///  Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +20,71 @@
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClockForm));
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnClockMouseDown);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.BackColor = System.Drawing.Color.White;
+            this.buttonClose.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.Font = new System.Drawing.Font("Script MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonClose.Location = new System.Drawing.Point(150, 270);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(50, 26);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Text = "close";
+            this.buttonClose.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.OnButtonCloseClick);
+            // 
+            // ClockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(350, 350);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ClockForm";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ClockForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private PictureBox pictureBox;
+        private Button buttonClose;
     }
 }
-
--- Homework7/Clock/Clock/Form1.cs
+++ Homework7/Clock/Clock/Form1.cs
@@ -1,20 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+namespace Clock;
 
-namespace Clock
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+/// <summary>
+/// Class for clock
+/// </summary>
+public partial class ClockForm : Form
 {
-    public partial class Form1 : Form
+    [DllImport("user32", CharSet = CharSet.Auto)]
+    internal extern static bool PostMessage(IntPtr hWnd, uint Msg, uint WParam, uint LParam);
+    [DllImport("user32", CharSet = CharSet.Auto)]
+    internal extern static bool ReleaseCapture();
+    public ClockForm()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        this.BackColor = Color.AliceBlue;
+        this.TransparencyKey = this.BackColor;
+        timer.Tick += new EventHandler(TimerTick);
+    }
+
+    private int centerFormX = 175, centerFormY = 175;
+    private int secondHandLength = 100, minuteHandLength = 90, hourHandLength = 75;
+
+    private Timer timer = new Timer();
+    private void OnClockMouseDown(object sender, MouseEventArgs e)
+    {
+        const uint WM_SYSCOMMAND = 0x0112;
+        const uint DOMOVE = 0xF012;
+        ReleaseCapture();
+        PostMessage(this.Handle, WM_SYSCOMMAND, DOMOVE, 0);
+    }
+
+    private void OnButtonCloseClick(object sender, EventArgs e)
+    {
+        this.Close();
+    }
+
+    private void ClockForm_Load(object sender, EventArgs e)
+    {
+        timer.Interval = 1000;
+        timer.Start();
+    }
+
+    private int[] MinuteOrSecondHandCoordination(int val, int length)
+    {
+        var coordination = new int[2];
+        val *= 6;
+        coordination[0] = centerFormX + (int)(length * Math.Sin(Math.PI * val / 180));
+        coordination[1] = centerFormY - (int)(length * Math.Cos(Math.PI * val / 180));
+        return coordination;
+    }
+    private int[] HourHandCoordination(int hval, int mval, int length)
+    {
+        var coordination = new int[2];
+        int val = (int)((hval * 30) + (mval * 0.5));
+        coordination[0] = centerFormX + (int)(length * Math.Sin(Math.PI * val / 180));
+        coordination[1] = centerFormY - (int)(length * Math.Cos(Math.PI * val / 180));
+        return coordination;
+    }
+    private void TimerTick(object? sender, EventArgs e)
+    {
+        int second = DateTime.Now.Second;
+        int minute = DateTime.Now.Minute;
+        int hour = DateTime.Now.Hour;
+
+        var handCoordination = new int[2];
+        Graphics graphics = pictureBox.CreateGraphics();
+
+        // Erasing previous hands
+        handCoordination = MinuteOrSecondHandCoordination(second - 1, secondHandLength + 4);
+        graphics.DrawLine(new Pen(Color.White, 50f), new Point(centerFormX, centerFormY), new Point(handCoordination[0], handCoordination[1]));
+        handCoordination = MinuteOrSecondHandCoordination(minute - 1, minuteHandLength + 4);
+        graphics.DrawLine(new Pen(Color.White, 50f), new Point(centerFormX, centerFormY), new Point(handCoordination[0], handCoordination[1]));
+        handCoordination = HourHandCoordination(hour % 12, minute, hourHandLength + 4);
+        graphics.DrawLine(new Pen(Color.White, 50f), new Point(centerFormX, centerFormY), new Point(handCoordination[0], handCoordination[1]));
+
+        // Draw new hands
+        handCoordination = HourHandCoordination(hour % 12, minute, hourHandLength);
+        graphics.DrawLine(new Pen(Color.Gray, 4f), new Point(centerFormX, centerFormY), new Point(handCoordination[0], handCoordination[1]));
+        handCoordination = MinuteOrSecondHandCoordination(minute, minuteHandLength);
+        graphics.DrawLine(new Pen(Color.Black, 3f), new Point(centerFormX, centerFormY), new Point(handCoordination[0], handCoordination[1]));
+        handCoordination = MinuteOrSecondHandCoordination(second, secondHandLength);
+        graphics.DrawLine(new Pen(Color.DarkOrange, 2f), new Point(centerFormX, centerFormY), new Point(handCoordination[0], handCoordination[1]));
     }
 }
